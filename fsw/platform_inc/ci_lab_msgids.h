8f4125504f65007e97e9a9a608a07124504b427f
Lucas, John P <John.P.Lucas@nasa.gov>
2023-07-05 20:36:33 -0400

    [nasa/nos3#88] Updates to conform with draco-rc4;

--- fsw/platform_inc/ci_lab_msgids.h
+++ fsw/platform_inc/ci_lab_msgids.h
@@ -23,9 +23,9 @@
 #ifndef CI_LAB_MSGIDS_H
 #define CI_LAB_MSGIDS_H
 
-#define CI_LAB_CMD_MID     0x1884
-#define CI_LAB_SEND_HK_MID 0x1885
+#define CI_LAB_CMD_MID     0x18E0
+#define CI_LAB_SEND_HK_MID 0x18E1
 
-#define CI_LAB_HK_TLM_MID 0x0884
+#define CI_LAB_HK_TLM_MID 0x08E0
 
 #endif
